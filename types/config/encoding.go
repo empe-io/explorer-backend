c9071ea432dabfd1d47b8191b3a22f678ab2508e
denerev <denerev68@gmail.com>
2025-02-03 15:45:00 +0100

    Update encoding and bump versions in go.mod

--- types/config/encoding.go
+++ types/config/encoding.go
@@ -3,19 +3,27 @@ package config
 import (
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	empeparams "github.com/empe-io/empe-chain/app/params"
 	"github.com/forbole/juno/v5/types/params"
 )
 
 // MakeEncodingConfig creates an EncodingConfig to properly handle all the messages
 func MakeEncodingConfig(managers []module.BasicManager) func() params.EncodingConfig {
 	return func() params.EncodingConfig {
-		encodingConfig := params.MakeTestEncodingConfig()
+		encodingConfig := empeparams.MakeEncodingConfig()
 		std.RegisterLegacyAminoCodec(encodingConfig.Amino)
 		std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 		manager := mergeBasicManagers(managers)
 		manager.RegisterLegacyAminoCodec(encodingConfig.Amino)
 		manager.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-		return encodingConfig
+
+		encodingCfg := params.EncodingConfig{
+			InterfaceRegistry: encodingConfig.InterfaceRegistry,
+			Codec:             encodingConfig.Marshaler,
+			TxConfig:          encodingConfig.TxConfig,
+			Amino:             encodingConfig.Amino,
+		}
+		return encodingCfg
 	}
 }
 
